7cf991d6e5c34fe5b200b02d42b2434e5b809b95
rashidjameel31 <30697180+rashidjameel31@users.noreply.github.com>
2017-08-14 14:04:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var rambo = document.getElementById("rj");
 rambo.innerHTML = 'there is a cahnge';
 var rambo1 = document.getElementById("rj1");
 rambo1.onclick = function() {
-    rambo1.style.marginleft="100px";
+    rambo1.style.marginleft="200px";
 };
